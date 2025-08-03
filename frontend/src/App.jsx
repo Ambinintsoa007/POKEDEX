a8fc4fed1935e4efad88a38fb664f19141b52e30
Njaka <hei.herinjaka@mail.com>
2025-08-03 15:38:35 +0300

    feat: initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const App = () => {
+  return <div className="container bg-red-500">
+    <div className="container flex justify-center-safe p-8 font-[1000]">herinjaka</div>
+  </div>;
+};
+
+export default App;
